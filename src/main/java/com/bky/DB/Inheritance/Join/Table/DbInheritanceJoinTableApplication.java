2644cddceffd7f3ab33974c535587f8ddf5177d5
Brijesh Yadav <briyadav@WKMZT366A8D9.global.publicisgroupe.net>
2023-12-20 18:55:36 +0530

    added bd config

---
+++ src/main/java/com/bky/DB/Inheritance/Join/Table/DbInheritanceJoinTableApplication.java
@@ -0,0 +1,13 @@
+package com.bky.DB.Inheritance.Join.Table;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbInheritanceJoinTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbInheritanceJoinTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bky/DB/Inheritance/Join/Table/DbInheritanceJoinTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bky.DB.Inheritance.Join.Table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbInheritanceJoinTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
